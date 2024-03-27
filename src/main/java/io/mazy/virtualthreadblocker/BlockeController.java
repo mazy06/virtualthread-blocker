3b9da097fca7b4f17bd288e3f3ca64485a6e9c84
Mazy Toufik <mazytoufik@outlook.com>
2024-03-27 01:09:31 +0100

    commit

--- src/main/java/io/mazy/virtualthreadblocker/BlockeController.java
+++ src/main/java/io/mazy/virtualthreadblocker/BlockeController.java
@@ -1,2 +1,22 @@
-package io.mazy.virtualthreadblocker;public class BlockeController {
+package io.mazy.virtualthreadblocker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+
+@RestController
+public class BlockeController {
+
+    private static final Logger log = LoggerFactory.getLogger(BlockeController.class);
+
+    @GetMapping("/block/{seconds}")
+    public void block(@PathVariable Integer seconds) throws InterruptedException {
+        Thread.sleep(Duration.ofMillis(seconds * 1000));
+        log.info("Blocked for {} seconds", seconds);
+    }
+
 }
--- src/main/java/io/mazy/virtualthreadblocker/VirtualthreadBlockerApplication.java
+++ src/main/java/io/mazy/virtualthreadblocker/VirtualthreadBlockerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VirtualthreadBlockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VirtualthreadBlockerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualthreadBlockerApplication.class, args);
+    }
 
 }
