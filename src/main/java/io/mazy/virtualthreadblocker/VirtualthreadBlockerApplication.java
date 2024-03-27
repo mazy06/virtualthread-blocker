564a80330226391b7227c8514309e4e54424968f
Mazy Toufik <mazytoufik@outlook.com>
2024-03-27 01:01:15 +0100

    Initial commit

---
+++ src/main/java/io/mazy/virtualthreadblocker/VirtualthreadBlockerApplication.java
@@ -0,0 +1,13 @@
+package io.mazy.virtualthreadblocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualthreadBlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualthreadBlockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mazy/virtualthreadblocker/VirtualthreadBlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mazy.virtualthreadblocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualthreadBlockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
